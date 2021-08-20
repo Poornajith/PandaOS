317442fb2f6fb0b819edcb190ea18a9c1f3d7331
Poornajith <63180161+Poornajith@users.noreply.github.com>
2021-08-20 18:56:49 +0530

    Update serial_port.c

--- serial_port.c
+++ serial_port.c
@@ -1,63 +1,29 @@
+#include "io.h"
 #include "serial_port.h"
 
 void serial_configure_baud_rate(unsigned short com, unsigned short divisor) {
-  /* Tell the serial port to first expect the highest 8 bits, then the lowest
-   * 8 bits. This is done by sending 0x80 to the line command port
-   */
   outb(SERIAL_LINE_COMMAND_PORT(com), SERIAL_LINE_ENABLE_DLAB);
-  outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00FF);
-  outb(SERIAL_DATA_PORT(com), divisor & 0x00FF);
+  outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00ff);
+  outb(SERIAL_DATA_PORT(com), divisor & 0x00ff);
 }
 
-void serial_configure_line(unsigned short com) {
-  /* Bit:     | 7 | 6 | 5 4 3 | 2 | 1 0 |
-   * Content: | d | b | prty  | s | dl  |
-   * Value:   | 0 | 0 | 0 0 0 | 0 | 1 1 | = 0x03
-   * data length of 8 bits, one stop bit, no parity bit, break control
-   * disabled and DLAB disabled
-   */
+void serial_configure_line(unsigned short com)
+{
   outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
 }
 
-void serial_configure_fifo_buffer(unsigned short com) {
-  /* Bit:     | 7 6 | 5  | 4 | 3   | 2   | 1   | 0 |
-   * Content: | lvl | bs | r | dma | clt | clr | e |
-   * Value:   | 1 1 | 0  | 0 | 0   | 1   | 1   | 1 | = 0xC7
-   */
-  outb(SERIAL_FIFO_COMMAND_PORT(com), 0xC7);
-}
-
-void serial_configure_modem(unsigned short com) {
-  /* Bit:     | 7 | 6 | 5  | 4  | 3   | 2   | 1   | 0   |
-   * Content: | r | r | af | lb | ao2 | ao1 | rts | dtr |
-   * Value:   | 0 | 0 | 0  | 0  | 0   | 0   | 1   | 1 | = 0x03
-   */
-  outb(SERIAL_MODEM_COMMAND_PORT(com), 0x03);
-}
-
-int serial_is_transmit_fifo_empty(unsigned short com) {
-  /* 0x20 = 0010 0000 */
+int serial_is_transmit_fifo_empty(unsigned int com) {
   return inb(SERIAL_LINE_STATUS_PORT(com)) & 0x20;
 }
 
-int serial_write(unsigned short com, char *buf, unsigned int len) {
-  unsigned int indexToBuffer = 0;
-  while (indexToBuffer < len) {
-    if (serial_is_transmit_fifo_empty(com)) {
-      serial_write_byte(com, buf[indexToBuffer]);
-      indexToBuffer++;
-    }
+void serial_write(char *buf, unsigned int len) {
+  serial_configure_baud_rate(SERIAL_COM1_BASE, 3);
+  serial_configure_line(SERIAL_COM1_BASE);
+
+  unsigned int i;
+  for (i=0; i<len; i++) {
+    while (!serial_is_transmit_fifo_empty(SERIAL_COM1_BASE));
+
+    outb(SERIAL_DATA_PORT(SERIAL_COM1_BASE), buf[i]);
   }
-  return 0;
-}
-
-void serial_write_byte(unsigned short port, char byteData) {
-  outb(port, byteData);
-}
-
-void serial_configure(unsigned short port, unsigned short baudRate) {
-  serial_configure_baud_rate(port, baudRate);
-  serial_configure_line(port);
-  serial_configure_fifo_buffer(port);
-  serial_configure_modem(port);
 }
