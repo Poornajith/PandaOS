f501e157490579b477b6d5055970fc6ac496d28d
Poornajith <63180161+Poornajith@users.noreply.github.com>
2021-09-10 21:03:13 +0530

    Update common.c

--- common.c
+++ common.c
@@ -1,1 +1,16 @@
 
+#ifndef COMMON_H
+#define COMMON_H
+
+typedef unsigned int   u32int;
+typedef unsigned char  u8int;
+
+void memset(u8int *dest, u8int val, u32int len);
+
+#endif 
+// Write len copies of val into dest.
+void memset(u8int *dest, u8int val, u32int len)
+{
+    u8int *temp = (u8int *)dest;
+    for ( ; len != 0; len--) *temp++ = val;
+}
